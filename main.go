cd50012726959981d5e0a8acc561e4831bc57348
jaydhelton <jaydhelton@gmail.com>
2020-07-25 19:47:27 -0500

    adding some color

--- main.go
+++ main.go
@@ -10,32 +10,47 @@ import (
 
 func walk(input string, deep bool, callback func(string)) error {
 	return filepath.Walk(input, func(path string, info os.FileInfo, err error) error {
+		if info != nil && info.IsDir() {
+			callback("\033[35m" + path + "\033[0m")
+
+			if input != path && !deep {
+				return filepath.SkipDir
+			}
+
+			return nil
+		}
 		callback(path)
+
 		return nil
 	})
 }
 
 func main() {
+	deepPtr := flag.Bool("deep", false, "check")
+
 	flag.Parse()
+
 	if flag.NArg() == 0 {
 		wd, err := os.Getwd()
 		if err != nil {
-			os.Exit(1)
+			panic(err)
 		}
-		err = walk(wd, false, func(path string) {
+		err = walk(wd, *deepPtr, func(path string) {
 			log.Println(path)
 		})
 
 		if err != nil {
-			os.Exit(1)
+			panic(err)
 		}
 
 	}
+
 	inputArg := flag.Arg(0)
 	pattern := flag.Arg(1)
-	err := walk(inputArg, true, func(path string) {
+
+	err := walk(inputArg, *deepPtr, func(path string) {
 		if strings.Contains(path, pattern) {
-			log.Println(path)
+			log.Println("\033[31m" + path + "\033[0m")
 		}
 	})
 
